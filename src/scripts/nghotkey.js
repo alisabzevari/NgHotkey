5c9f53a3cff1f06b0663cc7e717b4de6a34cdebb
Ali Sabzevari <alisabzevari@gmail.com>
2013-11-27 08:02:58 +0330

    Initial commit

---
+++ src/scripts/nghotkey.js
@@ -0,0 +1,37 @@
+﻿var nghotkey = angular.module('nghotkey', []);
+
+nghotkey.factory('Mousetrap', function () {
+    return {
+        bind: function (keys, callback, action) {
+            Mousetrap.bind(keys, callback, action);
+        },
+        unbind: function (keys, action) {
+            Mousetrap.unbind(keys, action);
+        },
+        trigger: function (keys, action) {
+            Mousetrap.trigger(keys, action);
+        },
+        reset: function () {
+            Mousetrap.reset();
+        },
+        stopCallbak: function (e, element) {
+            Mousetrap.stopCallback(e, element);
+        }
+    };
+});
+
+nghotkey.directive('hotkey', ['Mousetrap', function (mousetrap) {
+    return {
+        restrict: 'A',
+        link: function (scope, iElement, iAttrs) {
+            if (!iAttrs.hotkeyAction)
+                mousetrap.bind(iAttrs.hotkey, function () {
+                    iElement.focus();
+                });
+            else
+                mousetrap.bind(iAttrs.hotkey, function () {
+                    iElement.focus();
+                }, iAttrs.hotkeyAction);
+        }
+    };
+}]);
